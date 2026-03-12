ef89df1f6d9eaf1526b2f74e54c4d12220b60aac
WijekoonJanithBCS <wijekoonjanith72@gmail.com>
2026-03-12 18:34:05 +0530

    day-16-completed

--- i-computers-frontend/src/App.jsx
+++ i-computers-frontend/src/App.jsx
@@ -4,20 +4,24 @@ import AdminPage from "./pages/admin"
 import HomePage from "./pages/home"
 import LoginPage from "./pages/login"
 import TestPage from "./components/test"
-
+import {Toaster} from "react-hot-toast"
 
 function App() {
   
 
   return (
-    
-      
+      <div>
+      <Toaster position="top-right"/> 
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/admin/*" element={<AdminPage/>}/>
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/test" element={<TestPage/>}/>
         </Routes>
+      </div>
+    
+      
+       
         
       
     )
--- i-computers-frontend/src/pages/login.jsx
+++ i-computers-frontend/src/pages/login.jsx
@@ -1,15 +1,53 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+
+    function login(){
+        console.log(email)
+        console.log(password)
+        axios.post(import.meta.env.VITE_API_URL + "/users/login", {
+            email: email,
+            password: password
+        }).then((response)=>{
+            console.log(response)
+            if(response.data.role=="admin"){
+                
+                navigate("/admin/")
+            }
+            else{
+                //send to home page
+            }
+            //alert("Login successful")
+            toast.success("Login success")
+        }).catch((error)=>{
+            console.log(error)
+            //alert("login failed")
+            toast.error("Login failed")
+        })
+    }
+
     return(
         <div className="w-full h-screen bg-[url('/img01.jpg')] bg-cover bg-center no-repeat flex">
             <div className="w-[50%] h-full"></div>
             <div className="w-[50%] h-full flex justify-center items-center ">
                 <div className="bg-white w-[400px] h-[500px] rounded-lg flex flex-col justify-center items-center">
-                    <input type="email" placeholder="Email" className="m-5 p-3 w-[90%] h-[40px] rounded-lg border border-blue-600"/>
-                    <input type="password" placeholder="password" className="m-5 p-3 w-[90%] h-[40px] rounded-lg border border-blue-600"/>
+                    <input type="email" placeholder="Email" 
+                        onChange={(e)=>{
+                            setEmail(e.target.value)
+                        }
+                        } className="m-5 p-3 w-[90%] h-[40px] rounded-lg border border-blue-600"/>
+                    <input type="password" placeholder="password" 
+                        onChange={(e)=>
+                            setPassword(e.target.value)
+                        }className="m-5 p-3 w-[90%] h-[40px] rounded-lg border border-blue-600"/>
                     <p className="w-full text-right text-blue-700 pr-5">forgot password ? <Link to="/forgot-password" className="text-blue-600 font-bold">RESET</Link></p>
-                    <button className="m-5 p-3 w-[90%] h-[50px] rounded-lg bg-blue-700 text-white font-bold">LOGIN</button>
+                    <button onClick={login} className="m-5 p-3 w-[90%] h-[50px] rounded-lg bg-blue-700 text-white font-bold">LOGIN</button>
                     <button className="m-5 p-3 w-[90%] h-[50px] rounded-lg border border-blue-600 text-blue-600 font-bold">LOGIN WITH GOOGLE</button>
                     <p className="w-full text-right text-blue-700 pr-5">Dont have an account ? <Link to="/register" className="text-blue-600 font-bold">Register</Link></p>
                     
